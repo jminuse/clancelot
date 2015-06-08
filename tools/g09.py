Fixed bug in g09.neb - was setting start coords before Procrustes rotation

tools/g09.py
@@ -234,16 +234,16 @@ def neb(name, states, theory, extra_section='', k=0.1837): #Nudged Elastic Band.
 			NEB.theory = theory
 			NEB.k = k
 			
-			NEB.coords_start = []
-			for s in states[1:-1]:
-				for a in s:
-					NEB.coords_start += [a.x, a.y, a.z]
-			
 			from scipy.linalg import orthogonal_procrustes
 			for i in range(1,len(states)): #rotate all states to optimal alignment
 				rotation = orthogonal_procrustes([(a.x,a.y,a.z) for a in states[i]],[(a.x,a.y,a.z) for a in states[i-1]])[0]
 				for a in states[i]:
 					a.x,a.y,a.z = utils.matvec(rotation, (a.x,a.y,a.z))
+	
+			NEB.coords_start = []
+			for s in states[1:-1]:
+				for a in s:
+					NEB.coords_start += [a.x, a.y, a.z]
 	
 		@staticmethod
 		def calculate(coords):