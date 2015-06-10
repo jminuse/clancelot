4f4262149275f9e592314680923d759c7b7b41ef
Warren Crowell <crowell.warren@gmail.com>
2015-06-10 13:14:06 -0400

    reinstall prompt for y/n now no longer needs 'n\n' or 'y\n' but now accepts 'y' and 'n'

--- install.py
+++ install.py
@@ -223,10 +223,46 @@ f.write('''\n###############################################################
 f.close()
 
 downloaded_tarball=False
+
+#####The following is from http://code.activestate.com/recipes/134892/
+#####As opposed to raw_input() it does not require '\n' to end the input
+class _Getch:
+    """Gets a single character from standard input.  Does not echo to the screen."""
+    def __init__(self):
+        try:
+            self.impl = _GetchUnix()
+        except ImportError:
+            self.impl = _GetchWindows()
+
+    def __call__(self): return self.impl()
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+getch = _Getch()
+#####
+
 def reinstall(str):
 	while True:
 		print(str+' y/n:')
-		resp=raw_input()
+		resp=getch()
 		if resp=='y':
 			return True
 		if resp=='n':
