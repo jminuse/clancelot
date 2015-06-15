a8312a88ea3082d6e4f0cce738b44e6ad3b83dc6
Henry Herbol <hherbol@gmail.com>
2015-06-15 15:46:40 -0400

    Fixed chkg for recording data

--- tools/chkg.py
+++ tools/chkg.py
@@ -1,6 +1,6 @@
 import sys, log
 
 if (len(sys.argv)==2): log.chkg_E(sys.argv[1],record=False)
-if (len(sys.argv)==3): log.chkg_E(sys.argv[1],record=bool(int(sys.argv[2])))
-if (len(sys.argv)==4): log.chkg_E(sys.argv[1],record=bool(int(sys.argv[2])),unit=sys.argv[3])
+if (len(sys.argv)==3): log.chkg_E(sys.argv[1],unit=sys.argv[2],record=False)
+if (len(sys.argv)==4): log.chkg_E(sys.argv[1],unit=sys.argv[2],record=bool(int(sys.argv[3])))
 if (len(sys.argv)>4): print('Too many arguments')
