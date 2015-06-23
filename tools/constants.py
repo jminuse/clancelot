3c3d62c68cc058849abcd31283a77bc8240a2a25
Henry Herbol <hherbol@gmail.com>
2015-06-23 15:18:58 -0400

    Added units

--- tools/constants.py
+++ tools/constants.py
@@ -1,2 +1,2 @@
 # Everything is converted to Joules
-ENERGY = {'Ha':4.359744E-18,'eV':1.602E-19,'J':1.,'kcal/mol':4184./6.022E23,'kT_300':4.14195E-21}
+ENERGY = {'Ha':4.359744E-18,'eV':1.602E-19,'J':1.,'kcal/mol':4184./6.022E23,'kJ/mol':1000./6.022E23,'kT_300':4.14195E-21}
