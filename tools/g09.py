e5ead3f5213f62e57a31e2e23fc0493f64f1956d
Henry Herbol <hherbol@gmail.com>
2015-08-31 18:59:05 -0400

    Ditch changes in verlets_optimizer if TIME_STEP is reset to 0. Also, re-added old_gradient=new_gradient for end line.

--- tools/g09.py
+++ tools/g09.py
@@ -545,17 +545,20 @@ def neb(name, states, theory, extra_section='', procs=1, queue=None, spring_atom
 		TIME_STEP = 0.0
 		for step in range(100):
 			new_gradient = fprime(start)
-			if np.dot(velocity,new_gradient) > 0: TIME_STEP += 0.05
-			else: TIME_STEP = 0.0
+			if np.dot(velocity,new_gradient) >= 0: TIME_STEP += 0.05
+			else:
+				TIME_STEP = 0.0
+				new_gradient = old_gradient
 			velocity = (old_gradient+new_gradient)*0.5*TIME_STEP
 			start -= velocity
 			print("-----------------")
 			print("TIME_STEP = %lg" % TIME_STEP)
 			print velocity
 			print("-----------------")
+			old_gradient = new_gradient
 	
-	#verlets_optimizer(NEB.get_error, np.array(NEB.coords_start), fprime=NEB.get_forces)
-	verlet_optimizer(NEB.get_error, np.array(NEB.coords_start), fprime=NEB.get_forces)
+	#verlet_optimizer(NEB.get_error, np.array(NEB.coords_start), fprime=NEB.get_forces)
+	verlets_optimizer(NEB.get_error, np.array(NEB.coords_start), fprime=NEB.get_forces)
 
 def optimize_pm6(name, examples, param_string, starting_params, queue=None): #optimize a custom PM6 semi-empirical method based on Gaussian examples at a higher level of theory
 	from scipy.optimize import minimize
