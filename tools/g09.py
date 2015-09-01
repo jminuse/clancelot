Fixed major bug: perpendicular force calculation

tools/g09.py
@@ -450,8 +450,9 @@ def neb(name, states, theory, extra_section='', procs=1, queue=None, spring_atom
 					
 						#find DFT forces perpendicular to tangent
 						real_force = np.array( [b.fx,b.fz,b.fz] )
-						F_real_perpendicular = real_force - np.dot(real_force, tangent)
-					
+						#F_real_perpendicular = real_force - np.dot(real_force, tangent) #is this right?
+						F_real_perpendicular = real_force - np.dot(real_force, tangent)*tangent
+						
 						#set NEB forces
 						b.fx, b.fy, b.fz = F_spring_parallel + F_real_perpendicular
 			
@@ -531,8 +532,20 @@ def neb(name, states, theory, extra_section='', procs=1, queue=None, spring_atom
 			v = v_new
 			a = a_new
 
-	def quick_min_optimizer(f, start, nframes, fprime): #better, but tends to push error up eventually, especially towards endpoints. 
-		dt = 0.1
+	def vproj(v1, v2):
+		"""
+		Returns the projection of v1 onto v2
+		Parameters:
+		    v1, v2: numpy vectors
+		"""
+		mag2 = np.linalg.norm(v2)
+		if mag2 == 0:
+		    print "Can't project onto a zero vector"
+		    return v1
+		return v2 * (np.dot(v1, v2) / mag2)
+
+	def quick_min_optimizer(f, start, nframes, fprime):
+		dt = 0.05
 		max_dist = 0.2
 		r = start
 		v = np.array([0.0 for x in start])
@@ -540,19 +553,22 @@ def neb(name, states, theory, extra_section='', procs=1, queue=None, spring_atom
 
 		for step in range(1000):
 			forces = -fprime(r) # Get the forces
-			force_direction = forces/np.linalg.norm(forces) # Get the direction of the force
+			
+			#forces = -forces
+			
 			# Get the parallel velocity if it's in the same direction as the force
 			# Note, this must be frame based, not individual atom based or total based
 			natoms = len(v)/(3*(nframes-2))
-			print("\nNum Atoms = %lg\n" % natoms)
+			#print("\nNum Atoms = %lg\n" % natoms)
 			for i in range(1,nframes-1):
-				print("Frame %d of %d:" % (i,nframes)),
+				#print("Frame %d of %d:" % (i,nframes)),
 				low = (i-1)*natoms*3
 				high = i*natoms*3
 				if np.dot(v[low:high],forces[low:high]) > 0.0:
-					v[low:high] = np.dot(v[low:high],force_direction[low:high])*force_direction[low:high]
+					v[low:high] = vproj(v[low:high],forces[low:high])
 				else:
 					v[low:high] *= 0.0
+					print 'zeroed velocity for frame %d' % i
 				
 				# Euler step
 				v[low:high] += dt * forces[low:high]
@@ -560,14 +576,29 @@ def neb(name, states, theory, extra_section='', procs=1, queue=None, spring_atom
 				# Don't move atoms too far
 				if np.linalg.norm(v) > max_dist / dt:
 					v[low:high] = (max_dist / dt) * (v[low:high] / np.linalg.norm(v[low:high]))
+					print 'prevented from moving too fast in frame %d' % i
 
 				# Distance to move
 				dist = dt * v[low:high]
 
 				# Move atoms
 				r[low:high] += dist
-				print("%lg"% np.linalg.norm(v[low:high]))
-			print("\n-----------\n")
+				#print("%lg"% np.linalg.norm(v[low:high]))
+			#print("\n-----------\n")
+			
+			#prevent rotation or translation
+			coord_count = 0
+			for s in NEB.states[1:-1]:
+				for a in s:
+					a.x, a.y, a.z = r[coord_count], r[coord_count+1], r[coord_count+2]
+					coord_count += 3
+			utils.procrustes(NEB.states)
+			r = []
+			for s in states[1:-1]:
+				for a in s:
+					r += [a.x, a.y, a.z]
+			r = np.array(r)
+			
 
 	def order_2_optimizer(f, start, fprime): #better, but tends to push error up eventually, especially towards endpoints. 
 		old_gradient = np.array([0.0 for x in start]) 