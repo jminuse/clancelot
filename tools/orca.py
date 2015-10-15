db7c320b30a9273ac4e5cfbc2bb10a8a601a8070
James Stevenson <jminuse@gmail.com>
2015-10-15 19:54:02 -0400

    Working simple orca.py

--- tools/orca.py
+++ tools/orca.py
@@ -4,10 +4,10 @@ import utils, units, log, files
 
 def job(run_name, route, atoms=[], extra_section='', queue='batch', procs=1, charge_and_multiplicity='0 1', title='run by gaussian.py', blurb=None, eRec=True, force=False, previous=None, neb=[False,None,None,None], err=False, lint=False):
 	os.chdir('orca')
-	f = open(name+'.orca', 'w')
+	f = open(run_name+'.orca', 'w')
 	f.write(route+'\n*xyz '+charge_and_multiplicity+'\n')
 	for a in atoms:
 		f.write('%s %f %f %f\n' % (a.element, a.x, a.y, a.z) )
 	f.write('*\n')
-	os.system('/fs/home/jms875/build/orca/orca_3_0_2_linux_x86-64/orca %s.orca > %s.out &' % (name,name))
+	os.system('/fs/home/jms875/build/orca/orca_3_0_2_linux_x86-64/orca %s.orca > %s.out &' % (run_name,run_name))
 	os.chdir('..')
