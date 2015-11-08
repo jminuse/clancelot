c9570285286be39a7bd31ede82319a43c99bd4b4
Henry Herbol <hherbol@gmail.com>
2015-11-08 18:05:13 -0500

    Fixing install

--- install.py
+++ install.py
@@ -291,7 +291,7 @@ Popen('/fs/europa/g_pc/vmd-1.9 -e tmp.vmd', shell=True)\n''')
 if to_install['jsub']: f.write('complete -F _nbsAutoTab jsub\n\n')
 if to_install['jdel']: f.write('complete -F _jAutoTab jdel\n\n')
 if to_install['chkDFT']:
-	f.write("alias chkDFT='"+INSTALLDIR+"tools/chkDFT.py'\n")
+	f.write("alias chkDFT='python "+INSTALLDIR+"tools/chkDFT.py'\n")
 if to_install['merlin']: f.write("alias merlin='python -i "+INSTALLDIR+"tools/merlin.py'\n")
 if to_install['scanDFT']: f.write("\nalias scanDFT='python "+INSTALLDIR+"tools/scanDFT.py'\n")
 if to_install['prnt']: f.write('''alias prnt='function _prnt(){ssh asimov "lpr -P hplj4525-365 -o sides=two-sided-long-edge -o InputSlot=Tray2 $PWD/$1;logout";echo "Printed..."};_prnt'\n''')
