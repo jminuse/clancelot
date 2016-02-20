31b064f8072d4b061f14b9f8fd7507e975aa9062
Henry Herbol <hherbol@gmail.com>
2016-02-20 13:12:53 -0500

    Adding unit_testing for neb

---
+++ unit_tests/neb/pys_chk/5/CNK_KCN_FIRE.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append('/fs/home/hch54/clancelot/tools/')
+import files, neb
+
+fptr = 'CNK_KCN.xyz'
+frames = files.read_xyz('/fs/home/hch54/Documents/projects/BFGS_bench4/xyz/'+fptr)
+opt = 'FIRE'
+route = '! RI-B2PLYP D3BJ def2-TZVP def2-TZVP/C Grid3 FinalGrid5'
+
+run_name = fptr[:fptr.find('.xyz')] + '_' + opt
+neb.neb(run_name, frames, route, opt=opt, maxiter=1000, gtol=0.00183726383934, DFT='orca', alpha=0.1, dt=0.1, mem=40)
