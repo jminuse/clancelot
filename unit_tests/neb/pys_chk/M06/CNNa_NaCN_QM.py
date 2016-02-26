e878b92510c214a5a5111c9b3ce846ce8deaf99c
Henry Herbol <hherbol@gmail.com>
2016-02-26 11:19:02 -0500

    Adding rest of bench test

---
+++ unit_tests/neb/pys_chk/M06/CNNa_NaCN_QM.py
@@ -0,0 +1,10 @@
+import sys
+import files, neb
+
+fptr = 'CNNa_NaCN'
+frames = files.read_xyz('/fs/home/hch54/clancelot/unit_tests/neb/xyz/'+fptr+'.xyz')
+opt = 'QM'
+route = '! M062X def2-TZVP Grid3 FinalGrid5'
+
+run_name = fptr[:fptr.find('.xyz')] + '_' + opt
+neb.neb(run_name, frames, route, opt=opt, maxiter=1000, gtol=0.00183726383934, DFT='orca', alpha=0.1, dt=0.1, mem=40, Nmax=20)
---
+++ unit_tests/neb/pys_chk/RIB2PLYP/BOK_KBO_QM.py
@@ -0,0 +1,10 @@
+import sys
+import files, neb
+
+fptr = 'BOK_KBO'
+frames = files.read_xyz('/fs/home/hch54/clancelot/unit_tests/neb/xyz/'+fptr+'.xyz')
+opt = 'QM'
+route = '! RI-B2PLYP D3BJ def2-TZVP def2-TZVP/C Grid3 FinalGrid5'
+
+run_name = fptr[:fptr.find('.xyz')] + '_' + opt
+neb.neb(run_name, frames, route, opt=opt, maxiter=1000, gtol=0.00183726383934, DFT='orca', alpha=0.1, dt=0.1, mem=40, Nmax=20)
