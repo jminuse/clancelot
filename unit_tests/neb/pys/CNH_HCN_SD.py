0da63a6d72e0946304054b749f562833d1ccd42f
James Stevenson <jminuse@gmail.com>
2016-04-07 11:18:30 -0400

    Added more files needed by unit_tests

---
+++ unit_tests/neb/pys/CNH_HCN_SD.py
@@ -0,0 +1,10 @@
+import sys
+import files, neb
+
+fptr = 'CNH_HCN'
+frames = files.read_xyz('/fs/home/jms875/clancelot/unit_tests/neb/xyz/'+fptr+'.xyz')
+opt = 'SD'
+route = '! HF-3c Grid3 FinalGrid5'
+
+run_name = fptr[:fptr.find('.xyz')] + '_' + opt
+neb.neb(run_name, frames, route, opt=opt, maxiter=200, gtol=0.00183726383934, DFT='orca', alpha=0.1, dt=0.1, mem=40, Nmax=20)
