b38e7ea46c5fbdf11e77e6bc39495df25e0e94a1
Henry Herbol <hherbol@gmail.com>
2016-08-04 17:09:02 -0400

    Added my local lammps_mcsmrff to sysconst path

--- tools/sysconst.py
+++ tools/sysconst.py
@@ -3,3 +3,5 @@ orca_path = '/fs/europa/g_pc/orca_3_0_3_linux_x86-64/orca'
 vmd_path = '/fs/europa/g_pc/vmd/bin/vmd'
 ovito_path = '/fs/europa/g_pc/ovito-2.6.2-x86_64/bin/ovito'
 opls_path = '/fs/europa/g_pc/Forcefields/OPLS/oplsaa.prm'
+
+lammps_mcsmrff = '/fs/home/hch54/lammps/lammps-7Dec15/src/lmp_serial'
